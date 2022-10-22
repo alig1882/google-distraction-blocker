3badd55298e7c5c3e483265d40ca942449b6d828
Ali <alig1882@gmail.com>
2022-10-22 19:36:55 -0400

    google distraction blocker

---
+++ blocker.js
@@ -0,0 +1,14 @@
+const current_url = window.location.href;
+const blacklisted_queries = ['tbm=isch', 'tbm=nws', 'tbm=vid'];
+const has_blacklisted_queries = blacklisted_queries.some(blacklisted_value => current_url.includes(blacklisted_value));
+
+if (has_blacklisted_queries) window.location.replace('https://tailwindcss.com');
+
+const newsElements = document.getElementsByTagName('g-section-with-header');
+while (newsElements.length > 0) {
+    const element = newsElements[0];
+    element.remove();
+}
+
+const videoElement = document.querySelector('div[jscontroller="UzbKLd"]');
+videoElement?.remove();
