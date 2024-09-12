3fbd51df6aff2bd45f0901461ede093e0cbdf31b
Ali <alig1882@gmail.com>
2024-09-12 18:14:38 -0400

    committing all

--- blocker.js
+++ blocker.js
@@ -1,5 +1,5 @@
 const current_url = window.location.href;
-const blacklisted_queries = ['tbm=isch', 'tbm=nws', 'tbm=vid'];
+const blacklisted_queries = ['tbm=isch', 'tbm=nws', 'tbm=vid', 'udm=2'];
 const has_blacklisted_queries = blacklisted_queries.some(blacklisted_value => current_url.includes(blacklisted_value));
 
 if (has_blacklisted_queries) window.location.replace('https://tailwindcss.com');
